2337cf9d1f5803dbbdd277a38b4e600c9d10ac82
Sunatqayoom <115891537+Sunatqayoom@users.noreply.github.com>
2025-06-22 12:45:21 +0530

    Initial commit - Spring Boot Chat App

---
+++ src/main/java/com/chat/application/app/model/ChatMessage.java
@@ -0,0 +1,30 @@
+package com.chat.application.app.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private  String sender;
+    private  String content;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
